3a2fa808a4ecf21c2d0cecd6a53b07397dad2e5a
Lucas Santos <lukasaferra@gmail.com>
2024-06-03 23:10:21 -0300

    novoBaseURL

--- src/utils/HandleApi.js
+++ src/utils/HandleApi.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseURL = "http://localhost:5000";
+const baseURL = "https://projetoback-atrt.onrender.com";
 //"https://todo-app-express-backend-rtbt.onrender.com";
 
 
